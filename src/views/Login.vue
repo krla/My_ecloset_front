c6390e61bff3645d5f3a966d12b76c7630efa643
Abel Esquivel <abelesquivel94@gmail.com>
2020-04-28 18:17:18 +0100

    authFondo

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>
-    <h1 id="titulo" class="font-weight-thin">My eCloset</h1>
-    <v-container>
+  <div id="login">
+    <div id="content">
+      <h1 id="titulo" class="font-weight-thin">My eCloset</h1>
+      <v-container>
         <v-row class="ml-8">
           <v-form>
-            <v-text-field label="E-mail" v-model="email" :rules="emailRules" prepend-icon="mdi-email"></v-text-field>
+            <v-text-field
+              label="E-mail"
+              v-model="email"
+              :rules="emailRules"
+              prepend-icon="mdi-email"
+            ></v-text-field>
             <v-text-field
               label="Password"
               v-model="userPassword"
@@ -16,60 +22,76 @@
             ></v-text-field>
           </v-form>
         </v-row>
-    </v-container>
+      </v-container>
       <v-divider></v-divider>
       <v-card-actions>
-        <v-btn id="button" class="ma-2" color="info" @click="login" >
-          Login
-        </v-btn>
+        <v-btn id="button" color="#13978F" dark @click="login">Login</v-btn>
       </v-card-actions>
-      <v-card-actions>
-        <v-btn  to="/signup" id="button" class="ma-2" color="info">
-          Ir a Signup
-        </v-btn>
-      </v-card-actions>
+       <p>Si no estás registrado <router-link to="/signup">Pulse aqui</router-link></p>
+    </div>
   </div>
 </template>
 
 <script>
-import Api from '../services/Api'
+import Api from "../services/Api";
 
 export default {
-  data () {
+  data() {
     return {
       showPassword: false,
-      userPassword: '',
+      userPassword: "",
       passwordRule: [
-        v => !!v || 'Password is required',
-        v => v.length >= 10 || 'Password must be more than 10 characters'
+        v => !!v || "Password is required",
+        v => v.length >= 10 || "Password must be more than 10 characters"
       ],
-      email: '',
+      email: "",
       emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+        v => !!v || "E-mail is required",
+        v => /.+@.+\..+/.test(v) || "E-mail must be valid"
       ]
-    }
+    };
   },
   methods: {
-    login () {
+    login() {
       const user = {
         user_email: this.email,
         user_password: this.userPassword
-      }
+      };
       Api.login(user)
         .then(response => {
-          localStorage.setItem('token', response.token)
-          this.$router.push('/home')
+          localStorage.setItem("token", response.token);
+          this.$router.push("/home");
         })
-        .catch(err => console.log(err))
+        .catch(err => console.log(err));
     }
   }
-}
+};
 </script>
 
 <style lang='scss' scoped>
-#titulo{
+#login {
+  background-image: url("../assets/fondo_auth.png");
+  height: 100vh;
+}
+
+#content {
+  margin: auto;
+}
+
+#titulo {
+  text-align: center;
+}
+
+#button {
+  margin: auto;
+  margin-top: 10px;
+}
+p {
   text-align: center;
   margin-top: 10px;
+}
+#container {
+  margin-left: 15px;
+
 }
 </style>
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,99 +1,110 @@
 <template>
   <div id="signup">
+    <div id="content">
       <h1 id="titulo" class="font-weight-thin">My eCloset</h1>
-      <v-container>
+      <v-container id="container">
         <v-row class="ml-8">
-            <v-form>
-              <v-text-field
-                label="Nombre de usuario"
-                v-model="username"
-                prepend-icon="mdi-account-circle"
-                :rules="userRules"
-              ></v-text-field>
+          <v-form>
+            <v-text-field
+              label="Nombre de usuario"
+              v-model="username"
+              prepend-icon="mdi-account-circle"
+              :rules="userRules"
+            ></v-text-field>
 
-              <v-text-field label="E-mail" v-model="email" :rules="emailRules" prepend-icon="mdi-email"></v-text-field>
+            <v-text-field label="E-mail" v-model="email" :rules="emailRules" prepend-icon="mdi-email"></v-text-field>
 
-              <v-text-field
-                label="Password"
-                v-model="userPassword"
-                :type="showPassword ? 'text' : 'password'"
-                prepend-icon="mdi-lock"
-                :rules="passwordRule"
-                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                @click:append="showPassword = !showPassword"
-              ></v-text-field>
-              <v-radio-group v-model="genderInput">
-                <v-radio
-                  v-for="gender in genders"
-                  :key="gender"
-                  :label="gender"
-                  :value="gender"
-                ></v-radio>
-              </v-radio-group>
-            </v-form>
+            <v-text-field
+              label="Password"
+              v-model="userPassword"
+              :type="showPassword ? 'text' : 'password'"
+              prepend-icon="mdi-lock"
+              :rules="passwordRule"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="showPassword = !showPassword"
+            ></v-text-field>
+            <v-radio-group v-model="genderInput">
+              <v-radio v-for="gender in genders" :key="gender" :label="gender" :value="gender"></v-radio>
+            </v-radio-group>
+          </v-form>
         </v-row>
       </v-container>
-      <v-divider></v-divider>
       <v-card-actions>
-        <v-btn color="info" @click="signup">Signup</v-btn>
+        <v-btn id="buttom" color="#13978F" dark @click="signup">Signup</v-btn>
       </v-card-actions>
-      <v-card-actions>
-        <v-btn  to="/login" id="button" class="ma-2" color="info">
-          Ir a Login
-        </v-btn>
-      </v-card-actions>
+      <p>Si ya estás registrado <router-link to="/login">Pulse aqui</router-link></p>
+    </div>
   </div>
 </template>
 
 <script>
-import Api from '../services/Api'
+import Api from "../services/Api";
 
 export default {
-  data () {
+  data() {
     return {
       showPassword: false,
-      userPassword: '',
+      userPassword: "",
       passwordRule: [
-        v => !!v || 'Password is required',
-        v => v.length >= 10 || 'Password must be more than 10 characters'
+        v => !!v || "Password is required",
+        v => v.length >= 10 || "Password must be more than 10 characters"
       ],
-      username: '',
-      userRules: [v => !!v || 'Username is required'],
-      email: '',
+      username: "",
+      userRules: [v => !!v || "Username is required"],
+      email: "",
       emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+        v => !!v || "E-mail is required",
+        v => /.+@.+\..+/.test(v) || "E-mail must be valid"
       ],
       genderInput: "Mujer",
-      genders:["Mujer", "Hombre"]
-    }
+      genders: ["Mujer", "Hombre"]
+    };
   },
   methods: {
-    signup () {
+    signup() {
       const newUser = {
         user_name: this.username,
         user_email: this.email,
         user_password: this.userPassword,
         user_gender: this.genderInput
-      }
+      };
 
       Api.signup(newUser)
         .then(response => {
-          localStorage.setItem('token', response.token)
-          this.$router.push('/home')
+          localStorage.setItem("token", response.token);
+          this.$router.push("/home");
         })
-        .catch(err => console.log(err))
+        .catch(err => console.log(err));
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
+#signup {
+  background-image: url("../assets/fondo_auth.png");
+  height: 100vh;
+}
+
+#content {
+  margin: auto;
+}
+
 #titulo {
+  text-align: center;
+}
+
+
+#buttom {
+  margin: auto;
+  margin-top: 10px;
+}
+p {
   text-align: center;
   margin-top: 10px;
 }
-#signup {
-  background-image: url("../assets/fondo_auth.png");
+#container {
+  margin-left: 15px;
+
 }
 </style>
